8307af01702b6090c44f9c0b2282e4ec528b130c
Michael <Mlcorrieri@gmail.com>
2021-08-05 14:23:27 -0400

    fixed death/birth

--- src/ComposerCard.js
+++ src/ComposerCard.js
@@ -1,15 +1,18 @@
 import { Card } from "@material-ui/core";
 import React from "react";
+import { Link } from "@material-ui/core";
 
 function ComposerCard({ composer }) {
   const { id, name, complete_name, birth, death, portrait } = composer;
   return (
     <Card>
-      <h1>{name}</h1>
+      <Link>
+        <h1>{name}</h1>
+      </Link>
       <p>{complete_name}</p>
       <img src={portrait} alt="composer pic" />
-      <p>Born in: {birth}</p>
-      <p>Death: {death}</p>
+      <p>Born in: {birth.substring(0, 4)}</p>
+      <p>Death: {death.substring(0, 4)}</p>
     </Card>
   );
 }
