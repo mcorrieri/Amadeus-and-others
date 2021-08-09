22e4ab488b5f5d0a4a52a9f70ae9bbe91f9dbf98
Michael <Mlcorrieri@gmail.com>
2021-08-09 13:54:43 -0400

    handle clicks addedd for menu

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
 import { Button, Drawer, MenuItem } from "@material-ui/core";
+import { useHistory } from "react-router";
 
 function Header() {
   const [menu, setMenu] = useState(false);
   const [opendrawer, setOpenDrawer] = useState(false);
+  let history = useHistory();
+
+  function handleHomeClick() {
+    history.push("/");
+  }
+
+  function handleGenreClick() {
+    history.push("/genres");
+  }
+
+  function handlePeriodClick() {
+    history.push("/periods");
+  }
 
   function openDrawer() {
     setMenu(!menu);
@@ -18,9 +32,9 @@ function Header() {
       <h1>Welcome to The Classical Period</h1>
       <Button onClick={toggleDrawer}>Menu</Button>
       <Drawer open={openDrawer}>
-        <MenuItem>Home</MenuItem>
-        <MenuItem>Genres</MenuItem>
-        <MenuItem>Periods</MenuItem>
+        <MenuItem onClick={handleHomeClick}>Home</MenuItem>
+        <MenuItem onClick={handleGenreClick}>Genres</MenuItem>
+        <MenuItem onClick={handlePeriodClick}>Periods</MenuItem>
       </Drawer>
     </div>
   );
