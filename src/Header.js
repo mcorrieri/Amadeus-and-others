9a537832c098abdd6687c8d60813ac5feda657b0
Michael <Mlcorrieri@gmail.com>
2021-08-09 13:45:40 -0400

    menu displaying

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
-import { Drawer } from "@material-ui/core";
+import { Button, Drawer, MenuItem } from "@material-ui/core";
+
 function Header() {
-  const [menu, setMenu] = useState(null);
+  const [menu, setMenu] = useState(false);
+  const [opendrawer, setOpenDrawer] = useState(false);
+
+  function openDrawer() {
+    setMenu(!menu);
+  }
+
+  function toggleDrawer() {
+    setOpenDrawer(!opendrawer);
+  }
 
   return (
     <div className="Header">
       <h1>Welcome to The Classical Period</h1>
-      <Drawer></Drawer>
+      <Button onClick={toggleDrawer}>Menu</Button>
+      <Drawer open={openDrawer}>
+        <MenuItem>Home</MenuItem>
+        <MenuItem>Genres</MenuItem>
+        <MenuItem>Periods</MenuItem>
+      </Drawer>
     </div>
   );
 }
